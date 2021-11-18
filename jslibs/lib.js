d6981f4ca775557f631aba5d6493a357f7af2c7d
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-11-18 13:41:02 -0500

    Update lib.js

--- jslibs/lib.js
+++ jslibs/lib.js
@@ -3,12 +3,12 @@ var ace = import('https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.13/ace.js');
 
 var lib = {};
 
-lib.Name = "lib"
+lib.Name = "lib";
 
 lib.jsmediatags = jsmediatags;
 
 lib.ace = ace;
 
-console.log(lib)
+console.log(lib);
 
 window.lib = lib;
