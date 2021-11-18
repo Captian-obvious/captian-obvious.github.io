2f378df477de2b06b247fd6512c00f0076a4d3ff
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-11-18 11:45:56 -0500

    Update lib.js

--- jslibs/lib.js
+++ jslibs/lib.js
@@ -1,5 +1,5 @@
-import { jsmediatags } from 'https://cdnjs.cloudflare.com/ajax/libs/jsmediatags/3.9.5/jsmediatags.js'
-import { ace } from 'https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.13/ace.js'
+import * as jsmediatags from 'https://cdnjs.cloudflare.com/ajax/libs/jsmediatags/3.9.5/jsmediatags.js'
+import * as ace from 'https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.13/ace.js'
 
 var libsName = {}
 libsName.Name = "lib"
