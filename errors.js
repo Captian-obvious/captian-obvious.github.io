7267c0194592fc48a65d6e43d4619492924f8903
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-12-09 11:42:11 -0500

    Create errors.js

---
+++ errors.js
@@ -0,0 +1,8 @@
+var globals = {
+    replaceUrl: function (paramText) {
+        var newurl = window.location.protocol + '//' + window.location.host + window.location.pathname + '?' + paramText;
+        window.history.pushState({ path: newurl }, '', newurl);
+    },
+};
+
+window.globals = globals
