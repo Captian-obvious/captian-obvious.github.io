e3cd3707d6307144edb1b29cf21f7f6626deb85b
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-12-11 10:09:11 -0500

    Create require.js

---
+++ js/require.js
@@ -0,0 +1,14 @@
+window.require = function(src, ret){
+    var d = document.createElement('script');
+    d.src = src;
+    document.head.appendChild(d);
+    var fullURL = src.split('://');
+    var neededURL = fullURL[fullURl.length];
+    var nameParts = neededURL.split('/');
+    var fileName = nameParts[nameParts.length];
+    var g = fileName.split('.')
+    var global = g[0]
+    if (ret===true) {
+        return global
+    };
+};
