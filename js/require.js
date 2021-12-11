141e3262f70d710480a0f1351554a7a2714b4c49
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-12-11 10:17:51 -0500

    Update require.js

--- js/require.js
+++ js/require.js
@@ -1,9 +1,12 @@
 window.require = function(src, ret){
+    if (src==='jsmediatags') {
+        src = 'https://cdnjs.cloudflare.com/ajax/libs/jsmediatags/3.9.5/jsmediatags.js'
+    };
     var d = document.createElement('script');
     d.src = src;
     document.head.appendChild(d);
     var fullURL = src.split('://');
-    var neededURL = fullURL[fullURl.length];
+    var neededURL = fullURL[1];
     var nameParts = neededURL.split('/');
     var fileName = nameParts[nameParts.length];
     var g = fileName.split('.')
