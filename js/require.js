32545eaa77f5d36ebbde007519b24c76510427ec
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-12-11 10:22:45 -0500

    Update require.js

--- js/require.js
+++ js/require.js
@@ -1,6 +1,6 @@
 window.require = function(src, ret){
     if (src==='jsmediatags') {
-        src = 'https://cdnjs.cloudflare.com/ajax/libs/jsmediatags/3.9.5/jsmediatags.js'
+        src = 'https://cdnjs.cloudflare.com/ajax/libs/jsmediatags/3.9.5/jsmediatags.js';
     };
     var d = document.createElement('script');
     d.src = src;
@@ -8,9 +8,10 @@ window.require = function(src, ret){
     var fullURL = src.split('://');
     var neededURL = fullURL[1];
     var nameParts = neededURL.split('/');
-    var fileName = nameParts[nameParts.length];
-    var g = fileName.split('.')
-    var global = g[0]
+    var nameNUM = nameParts.length - 1;
+    var fileName = nameParts[nameNUM];
+    var g = fileName.split('.');
+    var global = g[0];
     if (ret===true) {
         return window[global]
     };
