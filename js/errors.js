f6c8f9f2490e49ff65e4c54f36325edce21b42af
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-12-12 15:27:47 -0500

    Update errors.js

--- js/errors.js
+++ js/errors.js
@@ -1,8 +1,16 @@
-var globals = {
-    replaceUrl: function(paramText) {
-        var newurl = window.location.protocol + '//' + window.location.host + window.location.pathname + '?' + paramText;
-        window.history.pushState({ path: newurl }, '', newurl);
-    },
+function detectQuery(name) {
+    var currentQuery = window.location.search;
+    if (currentQuery === "?"+name) {
+        return true;
+    }else{
+        return false;
+    };
 };
-
-window.g = globals;
+window.addEventListener('load', function() {
+    var is404 = detectQuery('code=404');
+    if (is404 === true) {
+        document.getElementById('page-container').innerHTML=`
+        
+        `
+    };
+});
