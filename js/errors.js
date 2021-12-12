707e202fed35be98901a987cf450f9bb20667ef2
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-12-12 15:29:16 -0500

    Update errors.js

--- js/errors.js
+++ js/errors.js
@@ -10,7 +10,10 @@ window.addEventListener('load', function() {
     var is404 = detectQuery('code=404');
     if (is404 === true) {
         document.getElementById('page-container').innerHTML=`
-        
+        <h1 style="color: #980000">Not Found (404)</h1>
+        <h4 style="color: #980000">The page you were looking for has been moved or deleted.</h4>
+        <img src="/images/404.png" width="120px" height="120px" />
+        <p style="color: #980000">click <a style="color: #ff0000" href="/"><em>here</em></a> to return home.</p>
         `
     };
 });
