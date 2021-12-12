49b019cbb5584a0ed3d44d421c5d0fb8f94ef0e9
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-12-12 15:51:20 -0500

    Update errors.js

--- js/errors.js
+++ js/errors.js
@@ -9,6 +9,7 @@ function detectQuery(name) {
 window.addEventListener('load', function() {
     var is404 = detectQuery('code=404');
     var is500 = detectQuery('code=500');
+    var is502 = detectQuery('code=502');
     var main = document.getElementById('page-container');
     if (is404 === true) {
         document.title = '404';
@@ -24,6 +25,15 @@ window.addEventListener('load', function() {
         main.innerHTML=`
         <h1 style="color: #980000">Server Error (500)</h1>
         <h4 style="color: #980000">The server encountered an error, please try again later.</h4>
+        <img src="/images/404.png" width="120px" height="120px" />
+        <p style="color: #980000">click <a style="color: #ff0000" href="/"><em>here</em></a> to return home.</p>
+        `
+    };
+    if (is502 === true) {
+        document.title = 'Gateway Error (502)';
+        main.innerHTML = `
+        <h1 style="color: #980000">Bad Gateway. (502)</h1>
+        <h4 style="color: #980000">Gateway error, please try again later.</h4>
         <img src="/images/404.png" width="120px" height="120px" />
         <p style="color: #980000">click <a style="color: #ff0000" href="/"><em>here</em></a> to return home.</p>
         `
