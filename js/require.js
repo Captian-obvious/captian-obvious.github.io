700fe51295bf7726bad3e1665bfd813ffa88c691
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2022-01-06 11:47:56 -0500

    Update require.js

--- js/require.js
+++ js/require.js
@@ -1,6 +1,11 @@
 window.require = function(src, ret){
+    var global = null
     if (src==='jsmediatags') {
         src = 'https://cdnjs.cloudflare.com/ajax/libs/jsmediatags/3.9.5/jsmediatags.js';
+    };
+    if (src==='dropzone') {
+        src='https://unpkg.com/dropzone@5/dist/min/dropzone.min.js'
+        global = 'Dropzone'
     };
     var d = document.createElement('script');
     d.src = src;
@@ -11,7 +16,9 @@ window.require = function(src, ret){
     var nameNUM = nameParts.length - 1;
     var fileName = nameParts[nameNUM];
     var g = fileName.split('.');
-    var global = g[0];
+    if (global===null) {
+        global = g[0];
+    };
     if (ret===true) {
         return window[global]
     };
