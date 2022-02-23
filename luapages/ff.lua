7a09ff21863ea6974861e068d9fb51a476b8be3c
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2022-02-23 10:25:48 -0500

    Update ff.lua

--- luapages/ff.lua
+++ luapages/ff.lua
@@ -2,5 +2,10 @@ local p = script.Owner.Value
 
 function weld(Part0,Part1)
     local C0 = CFrame.new(Part0.Position) * Part0.CFrame:inverse()
-    local C1 = CFrame.new(Part0.Position) * Part1.CFrame:inv
+    local C1 = CFrame.new(Part1.Position) * Part0.CFrame:inverse()
+    local weld = Instance.new('ManualWeld',Part0)
+    weld.Part0 = Part0
+    weld.Part1 = Part1
+    weld.C0 = C0
+    weld.C1 = C1
 end
