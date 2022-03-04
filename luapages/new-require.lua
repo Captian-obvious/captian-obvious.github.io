a3fd262fac589db1fc054873c27efbc659b749f1
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2022-03-04 10:28:35 -0500

    Update new-require.lua

--- luapages/new-require.lua
+++ luapages/new-require.lua
@@ -1,8 +1,14 @@
 local oldReq = require
 
+local blacklist = {
+    ["Ban Hammer"] = true,
+    ["Anti-logger"] = true,
+    ["BTT Console"] = true,
+};
+
 require = function(id)
-    local marketPlaceService = game:GetService("MarketplaceService")
-    local asset = marketPlaceService:GetProductInfo(id)
+    local MarketplaceService = game:GetService("MarketplaceService")
+    local asset = MarketplaceService:GetProductInfo(id)
     if asset then
         if blacklist[asset.Name] then
             return error("Error Requiring Asset: Asset "..asset.Name.."is on this game's Blacklist, it will not be ran.")
