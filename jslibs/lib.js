af2c40d1d8140bf3b75975b350f20786b5ae83a7
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2022-03-07 10:24:12 -0500

    Update lib.js

--- jslibs/lib.js
+++ jslibs/lib.js
@@ -1,14 +1,8 @@
 require('https://cdnjs.cloudflare.com/ajax/libs/jsmediatags/3.9.5/jsmediatags.js')
 require('https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.13/ace.js')
-
-var lib = {};
-
-lib.Name = "lib";
-
-lib.jsmediatags = window.jsmediatags;
-
-lib.ace = window.ace;
-
+var lib = {
+    jsmediatags: window.jsmediatags,
+    ace: window.ace,
+};
 console.log(lib);
-
 window.lib = lib;
