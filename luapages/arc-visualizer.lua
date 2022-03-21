79d7be8bb123b98d9eb9fd2b61371573e9aa6f8d
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2022-03-21 10:53:35 -0400

    Update arc-visualizer.lua

--- luapages/arc-visualizer.lua
+++ luapages/arc-visualizer.lua
@@ -3,7 +3,8 @@ local createArc = require(id)
 local maxPL = 0
 
 function createCircle(pos,r,num)
+    local angle_step = math.pi*2
     for i=1,num do
-        
+        math.cos(i)*(10)+x, math.sin(i)*(10)+y
     end
 end
