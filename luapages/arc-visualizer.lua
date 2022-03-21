902f87a1d6ff33076878b7231ef589df69b1aa56
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2022-03-21 11:13:44 -0400

    Update arc-visualizer.lua

--- luapages/arc-visualizer.lua
+++ luapages/arc-visualizer.lua
@@ -2,9 +2,14 @@ local id = "" -- module id--
 local createArc = require(id)
 local maxPL = 0
 
-function createCircle(pos,r,num)
-    local angle_step = math.pi*2
+function createCircle(pos,r,num) --creates a circle of CFrames and Vector3 positions
+    local angle_step = (math.pi*2)/num
+    local t = {}
     for i=1,num do
-        math.cos(i)*(10)+x, math.sin(i)*(10)+y
+        local angle = angle_step * i
+        local newPos = Vector3.new(pos.X+math.cos(angle)*r,pos.Y,pos.Z+math.sin(angle)*r)
+        local newCF = CFrame.new(pos, newPos)
+        t[i] = {CFrame = newCF, Position = newPos}
     end
+    return t
 end
