7cd63346dfe207a38f52bf5c92387001c094337e
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2022-03-23 10:46:10 -0400

    Update ft.lua

--- luapages/ft.lua
+++ luapages/ft.lua
@@ -1,1 +1,9 @@
-math.cos(x*math.pi*t)*d*t
+function int(func, start, stop, delta)
+    local delta = delta or 1e-4
+    local v = 0
+    for i = start, stop, delta do
+        i = i + func(v)*delta
+    end
+    return i
+end
+int(math.cos(x*math.pi*t)*d*t
