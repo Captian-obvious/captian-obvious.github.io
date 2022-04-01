fa84bf68161380d76b5d71d0fa36e431ee4a3a1b
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2022-04-01 11:28:20 -0400

    Update ft.lua

--- luapages/ft.lua
+++ luapages/ft.lua
@@ -2,7 +2,7 @@ function fft(size, func, x)
     local t = {}
     local v = 0
     for n=1, size do
-        v += func[n]*math.sin(2*math.pi* (n/size) * x * size)
+        v += func[n]*math.sin(2*math.pi*(n/size)*x* size)
         t[n] = v
     end
 end
