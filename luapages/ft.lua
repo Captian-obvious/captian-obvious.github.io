0be9dcd5f323a09a2d8f1ea2cd3e65f2d7a13660
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2022-04-01 11:19:47 -0400

    Update ft.lua

--- luapages/ft.lua
+++ luapages/ft.lua
@@ -1,1 +1,12 @@
-math.sin(2*math.pi* (n/64) * x*64)
+function sum(t)
+    local sum = 0
+    for k,v in pairs(t) do
+        sum = sum + v
+    end
+    return sum
+end
+function fft(size, func, x, endval)
+    for n=1, endval do
+        math.sin(2*math.pi* (n/size) * x * size)
+    end
+end
