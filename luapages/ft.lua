9e198d0bd33183e8bb2316a9843fa22754c8a124
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2022-04-04 10:14:24 -0400

    Update ft.lua

--- luapages/ft.lua
+++ luapages/ft.lua
@@ -1,9 +1,12 @@
 function fft(size, func, x)
     local t = {}
     local v = 0
-    for n=1, size do
+    local max = 0
+    for n=0, size-1 do
         v = v + func(n)
-        t[n] = 10e5*(v*func(n/size)*(math.cos(2*math.pi*(n/size)*x* size))) * 255
+        local l = 10e12*(v*func(n/size)*(math.cos(2*math.pi*(n/size)*x* size)))
+        max = math.max(max,l)
+        t[n] = ((10e12*(v*func(n/size)*(math.cos(2*math.pi*(n/size)*x* size))))/max)*255
     end
     return t
 end
@@ -13,11 +16,12 @@ function testfunction(x)
 end
 
 function work()
-    n=5
+    n=1
     for i=1,n do
         local e = fft(64,testfunction,i)
         for c=1,#e do
-            print(""..e[c])
+            print(""..math.sqrt(e[c]^2))
         end
     end
 end
+work()
