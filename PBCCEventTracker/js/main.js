7605d45f134b17b5f511363c95ba15bb408d857d
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2022-11-23 11:50:49 -0500

    Update main.js

--- PBCCEventTracker/js/main.js
+++ PBCCEventTracker/js/main.js
@@ -1,1 +1,10 @@
-
+function createAudio(url){
+    var src = url
+    var audio = new Audio()
+    if (src != null) {
+        audio.src=src
+    }
+    if (audio != null) {
+        return audio
+    }
+}
